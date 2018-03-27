4f6b030e1f26aaca7432e011abafb1add0de0315
Adam <adam@adamhammad.com>
2018-03-28 00:13:43 +1100

    Update

--- src/app/app.settings.ts
+++ src/app/app.settings.ts
@@ -7,6 +7,32 @@ import { NgModule } from '@angular/core';
     ],
   })
 
+/*
+
+   _____       ___ ________
+  / ___/____  / (_) __/ __/
+  \__ \/ __ \/ / / /_/ /_  
+ ___/ / /_/ / / / __/ __/  
+/____/ .___/_/_/_/ /_/     
+    /_/                    
+
+  Spliff 0.1 by Adam Hammad 2018
+  http://www.adamhammad.com
+  
+  Application Settings
+  - API_ENDPOINT: WordPress API URL
+    
+  It's generally your URL with /wp-json/ at the end.
+  e.g.
+  http://yourdomain.com/wp-json/
+
+  The trailing / is important, as the service is set up
+  to receive a URL with that on the end.
+
+*/
+
 export class AppSettings {
+
     public static API_ENDPOINT='http://localhost:8888/wp-json/';
+
   }
